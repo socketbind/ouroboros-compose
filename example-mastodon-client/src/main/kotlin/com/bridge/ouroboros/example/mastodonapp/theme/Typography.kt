8ab1b93af84ca73a98ed800e69442f01a8cd811d
Gabriel Miklós <socketbind@gmail.com>
2022-12-10 15:54:20 +0100

    Fix some warnings. Add Mastodon app skeleton.

---
+++ example-mastodon-client/src/main/kotlin/com/bridge/ouroboros/example/mastodonapp/theme/Typography.kt
@@ -0,0 +1,83 @@
+package com.bridge.ouroboros.example.mastodonapp.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val AppTypography = Typography(
+    h1 = TextStyle(
+        fontSize = 96.sp,
+        fontWeight = FontWeight.Light,
+        lineHeight = 117.sp,
+        letterSpacing = (-1.5).sp
+    ),
+    h2 = TextStyle(
+        fontSize = 60.sp,
+        fontWeight = FontWeight.Light,
+        lineHeight = 73.sp,
+        letterSpacing = (-0.5).sp
+    ),
+    h3 = TextStyle(
+        fontSize = 48.sp,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 59.sp
+    ),
+    h4 = TextStyle(
+        fontSize = 30.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 37.sp
+    ),
+    h5 = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 29.sp
+    ),
+    h6 = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 24.sp
+    ),
+    subtitle1 = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp
+    ),
+    subtitle2 = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        lineHeight = 17.sp,
+        letterSpacing = 0.1.sp
+    ),
+    body1 = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Light,
+        lineHeight = 22.sp,
+        letterSpacing = 0.15.sp
+    ),
+    body2 = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp
+    ),
+    button = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 16.sp,
+        letterSpacing = 1.25.sp
+    ),
+    caption = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 16.sp,
+        letterSpacing = 0.sp
+    ),
+    overline = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.SemiBold,
+        lineHeight = 16.sp,
+        letterSpacing = 1.sp
+    )
+)
